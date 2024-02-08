83c67d6b56eedac289194ed5564672a9fed33558
hiromisugie <lozz.gggg@gmail.com>
2024-02-08 16:18:02 +0900

    コールバック形式・エラーありのファイルを作成

---
+++ 03.asynchronous/my-asynchronous-callback-error.js
@@ -0,0 +1,20 @@
+import sqlite3 from "sqlite3";
+const db = new sqlite3.Database(':memory:');
+
+db.run("CREATE TABLE numbers(id INTEGER PRIMARY KEY AUTOINCREMENT)", function() {
+    db.run("INSERT INTO numbers(non_existent_column) VALUES(?)", [1], function(err) {
+        if (err) {
+            console.error(`レコード追加時のエラー: ${err.message}`);
+        } else {
+            console.log(`次のIDが自動採番されました: ${this.lastID}`);
+        }
+        db.get("SELECT non_existent_column FROM numbers", function(err, row) {
+            if (err) {
+                console.error(`レコード取得時のエラー: ${err.message}`);
+            } else {
+                console.log(`次のIDが取得されました: ${row.id}`);
+            }
+            db.close();
+        });
+    });
+});
