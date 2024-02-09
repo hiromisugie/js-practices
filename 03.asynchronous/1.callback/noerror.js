6f49cffdbf9f61f4e64f76753465193d82466466
hiromisugie <lozz.gggg@gmail.com>
2024-02-09 16:41:22 +0900

    npm run fixで自動修正

--- 03.asynchronous/1.callback/noerror.js
+++ 03.asynchronous/1.callback/noerror.js
@@ -1,13 +1,20 @@
 import sqlite3 from "sqlite3";
-const db = new sqlite3.Database(':memory:');
+const db = new sqlite3.Database(":memory:");
 
-db.run("CREATE TABLE numbers(id INTEGER PRIMARY KEY AUTOINCREMENT)", function() {
-    db.run("INSERT INTO numbers DEFAULT VALUES", function() {
-        console.log(`次のIDが自動採番されました: ${this.lastID}`);
-        db.each("SELECT * FROM numbers", function(_err, row) {
-            console.log(`次のIDが取得されました: ${row.id}`);
-        }, function() {
-            db.close();
-        });
+db.run(
+  "CREATE TABLE numbers(id INTEGER PRIMARY KEY AUTOINCREMENT)",
+  function () {
+    db.run("INSERT INTO numbers DEFAULT VALUES", function () {
+      console.log(`次のIDが自動採番されました: ${this.lastID}`);
+      db.each(
+        "SELECT * FROM numbers",
+        function (_err, row) {
+          console.log(`次のIDが取得されました: ${row.id}`);
+        },
+        function () {
+          db.close();
+        },
+      );
     });
-});
+  },
+);
--- 03.asynchronous/2.Promise/noerror.js
+++ 03.asynchronous/2.Promise/noerror.js
@@ -2,58 +2,65 @@ import sqlite3 from "sqlite3";
 
 // データベースを開く
 function openDatabase() {
-    return new Promise(function(resolve) {
-        const db = new sqlite3.Database(':memory:', function() {
-            resolve(db);
-        });
+  return new Promise(function (resolve) {
+    const db = new sqlite3.Database(":memory:", function () {
+      resolve(db);
     });
+  });
 }
 
 // テーブルを作成する
 function createTable(db) {
-    return new Promise(function(resolve) {
-        db.run("CREATE TABLE numbers(id INTEGER PRIMARY KEY AUTOINCREMENT)", function() {
-            resolve(db);
-        });
-    });
+  return new Promise(function (resolve) {
+    db.run(
+      "CREATE TABLE numbers(id INTEGER PRIMARY KEY AUTOINCREMENT)",
+      function () {
+        resolve(db);
+      },
+    );
+  });
 }
 
 // レコードを追加し、自動裁判されたIDを標準出力に出力する
 function insertRecord(db) {
-    return new Promise(function(resolve) {
-        db.run("INSERT INTO numbers DEFAULT VALUES", function() {
-            console.log(`次のIDが自動採番されました: ${this.lastID}`);
-            resolve(db);
-        });
+  return new Promise(function (resolve) {
+    db.run("INSERT INTO numbers DEFAULT VALUES", function () {
+      console.log(`次のIDが自動採番されました: ${this.lastID}`);
+      resolve(db);
     });
+  });
 }
 
 // レコードを取得し、IDを標準出力に出力する
 function selectRecord(db) {
-    return new Promise(function(resolve) {
-        db.each("SELECT * FROM numbers", function(_err, row) {
-            console.log(`次のIDが取得されました: ${row.id}`);
-        }, function() {
-            resolve(db);
-        });
-    });
+  return new Promise(function (resolve) {
+    db.each(
+      "SELECT * FROM numbers",
+      function (_err, row) {
+        console.log(`次のIDが取得されました: ${row.id}`);
+      },
+      function () {
+        resolve(db);
+      },
+    );
+  });
 }
 
 // データベースを閉じる
 function closeDatabase(db) {
-    return new Promise(function(resolve) {
-        db.close(function() {
-            resolve();
-        });
+  return new Promise(function (resolve) {
+    db.close(function () {
+      resolve();
     });
+  });
 }
 
 function main() {
-    openDatabase()
-        .then(createTable)
-        .then(insertRecord)
-        .then(selectRecord)
-        .then(closeDatabase)
+  openDatabase()
+    .then(createTable)
+    .then(insertRecord)
+    .then(selectRecord)
+    .then(closeDatabase);
 }
 
 main();
