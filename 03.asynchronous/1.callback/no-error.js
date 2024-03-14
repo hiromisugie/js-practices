9098810c4a33cfa107b1ebb46492d86ca82b5fc3
hiromisugie <lozz.gggg@gmail.com>
2024-03-15 06:20:41 +0900

    function式でなくアロー関数を使用

--- 03.asynchronous/1.callback/no-error.js
+++ 03.asynchronous/1.callback/no-error.js
@@ -4,15 +4,15 @@ const db = new sqlite3.Database(":memory:");
 
 db.run(
   "CREATE TABLE numbers(id INTEGER PRIMARY KEY AUTOINCREMENT)",
-  function () {
+  () => {
     db.run("INSERT INTO numbers DEFAULT VALUES", function () {
       console.log(`次のIDが自動採番されました: ${this.lastID}`);
       db.each(
         "SELECT * FROM numbers",
-        function (_err, row) {
+        (_err, row) => {
           console.log(`次のIDが取得されました: ${row.id}`);
         },
-        function () {
+        () => {
           db.close();
         },
       );
