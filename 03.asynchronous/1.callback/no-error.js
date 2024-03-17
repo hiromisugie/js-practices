83b23b1b30294b9fa1754f6474e782f630fda6ba
hiromisugie <lozz.gggg@gmail.com>
2024-03-18 06:00:49 +0900

    テーブルのスキーマを見直して、booksテーブルに修正

--- 03.asynchronous/1.callback/no-error.js
+++ 03.asynchronous/1.callback/no-error.js
@@ -3,14 +3,14 @@ import sqlite3 from "sqlite3";
 const db = new sqlite3.Database(":memory:");
 
 db.run(
-  "CREATE TABLE numbers(id INTEGER PRIMARY KEY AUTOINCREMENT)",
+  "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
   () => {
-    db.run("INSERT INTO numbers DEFAULT VALUES", function () {
-      console.log(`次のIDが自動採番されました: ${this.lastID}`);
+    db.run("INSERT INTO books(title) VALUES(?)", "独習JavaScript", function () {
+      console.log(`新しい本が追加されました: ${this.lastID}`);
       db.each(
-        "SELECT * FROM numbers",
+        "SELECT * FROM books",
         (_err, row) => {
-          console.log(`次のIDが取得されました: ${row.id}`);
+          console.log(`取得した本: ${row.title}`);
         },
         () => {
           db.close();
