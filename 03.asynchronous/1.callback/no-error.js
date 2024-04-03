62160f07a63884955a010b40dbc5f4c55d9db00b
hiromisugie <lozz.gggg@gmail.com>
2024-04-04 06:03:35 +0900

    改行位置がおかしいのを修正

--- 03.asynchronous/1.callback/no-error.js
+++ 03.asynchronous/1.callback/no-error.js
@@ -7,15 +7,12 @@ db.run(
   () => {
     db.run("INSERT INTO books(title) VALUES(?)", "独習JavaScript", function () {
       console.log(`新しい本が追加されました: ${this.lastID}`);
-      db.get(
-        "SELECT * FROM books",
-        (_err, row) => {
-          if (row) {
-            console.log(`取得した本: ${row.title}`);
-          }
-          db.close();
-        },
-      );
+      db.get("SELECT * FROM books", (_err, row) => {
+        if (row) {
+          console.log(`取得した本: ${row.title}`);
+        }
+        db.close();
+      });
     });
   },
 );
