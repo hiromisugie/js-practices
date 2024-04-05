1c1ebe27c7b34a23f1603922ab4ab37b14844c87
hiromisugie <lozz.gggg@gmail.com>
2024-04-06 04:43:02 +0900

    no-error版のPromise修正を踏まえて、no-error版のasync/awaitを修正

--- 03.asynchronous/3.async-await/no-error.js
+++ 03.asynchronous/3.async-await/no-error.js
@@ -1,19 +1,22 @@
-import { db, runPromise, getAllPromise } from "../db-operations.js";
+import sqlite3 from "sqlite3";
+import { runPromise, getPromise, closePromise } from "../db-operations.js";
 
-async function main() {
-  try {
-    await runPromise(
-      "CREATE TABLE numbers(id INTEGER PRIMARY KEY AUTOINCREMENT)",
-    );
-    const id = await runPromise("INSERT INTO numbers DEFAULT VALUES");
-    console.log(`次のIDが自動採番されました: ${id}`);
-    const rows = await getAllPromise("SELECT * FROM numbers");
-    rows.forEach(function (row) {
-      console.log(`次のIDが取得されました: ${row.id}`);
-    });
-  } finally {
-    db.close();
+const db = new sqlite3.Database(":memory:");
+
+try {
+  await runPromise(
+    db,
+    "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  );
+  const result = await runPromise(db, "INSERT INTO books(title) VALUES(?)", [
+    "独習JavaScript",
+  ]);
+  console.log(`新しい本が追加されました: ${result.lastID}`);
+
+  const row = await getPromise(db, "SELECT * FROM books");
+  if (row) {
+    console.log(`取得した本: ${row.title}`);
   }
+} finally {
+  await closePromise(db);
 }
-
-main();
