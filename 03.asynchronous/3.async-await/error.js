0ee8e67f3cd50b24d7c11297f12b2e6fff84c835
hiromisugie <lozz.gggg@gmail.com>
2024-04-09 05:57:26 +0900

    意図しないエラーが発生した場合に再送出するコードを追加

--- 03.asynchronous/3.async-await/error.js
+++ 03.asynchronous/3.async-await/error.js
@@ -8,22 +8,26 @@ try {
     db,
     "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
   );
-  // 意図的にエラーを発生させるために、nullを挿入する
-  const result = await runPromise(db, "INSERT INTO books(title) VALUES(?)", [
-    null,
-  ]);
-  console.log(`新しい本が追加されました: ${result.lastID}`);
-} catch (err) {
-  console.error(`新しい本を追加する時のエラー: ${err.message}`);
-}
-try {
-  // 意図的にエラーを発生させるために、存在しないusersを参照する
-  const row = await getPromise(db, "SELECT * FROM users");
-  if (row) {
-    console.log(`取得した本: ${row.title}`);
+  try {
+    // 意図的にエラーを発生させるために、nullを挿入する
+    const result = await runPromise(db, "INSERT INTO books(title) VALUES(?)", [
+      null,
+    ]);
+    console.log(`新しい本が追加されました: ${result.lastID}`);
+  } catch (err) {
+    console.error(`新しい本を追加する時のエラー: ${err.message}`);
   }
+  try {
+    // 意図的にエラーを発生させるために、存在しないusersを参照する
+    const row = await getPromise(db, "SELECT * FROM users");
+    console.log(`取得した本: ${row.title}`);
+  } catch (err) {
+    console.error(`本を取得する時のエラー: ${err.message}`);
+  }
+  // eslint-disable-next-line no-useless-catch
 } catch (err) {
-  console.error(`本を取得する時のエラー: ${err.message}`);
+  // 意図しないエラーが発生した場合に再送出する
+  throw err;
 } finally {
   await closePromise(db);
 }
