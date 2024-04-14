12df859180242fc756a347ed11c0d4c42966fc52
hiromisugie <lozz.gggg@gmail.com>
2024-04-14 16:33:34 +0900

    エラーが起こる箇所のみtry...catchで囲み、想定するエラーとそうでないエラーとで処理を分岐

--- 03.asynchronous/3.async-await/error.js
+++ 03.asynchronous/3.async-await/error.js
@@ -3,33 +3,37 @@ import { runPromise, getPromise, closePromise } from "../db-operations.js";
 
 const db = new sqlite3.Database(":memory:");
 
+await runPromise(
+  db,
+  "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+);
 try {
-  await runPromise(
+  // 意図的にエラーを発生させるために、nullを挿入する
+  const result = await runPromise(
     db,
-    "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+    "INSERT INTO books(title) VALUES(?)",
+    null,
   );
-  try {
-    // 意図的にエラーを発生させるために、nullを挿入する
-    const result = await runPromise(
-      db,
-      "INSERT INTO books(title) VALUES(?)",
-      null,
-    );
-    console.log(`新しい本が追加されました: ${result.lastID}`);
-  } catch (err) {
-    console.error(`新しい本を追加する時のエラー: ${err.message}`);
-  }
-  try {
-    // 意図的にエラーを発生させるために、存在しないusersを参照する
-    const row = await getPromise(db, "SELECT * FROM users");
-    console.log(`取得した本: ${row.title}`);
-  } catch (err) {
-    console.error(`本を取得する時のエラー: ${err.message}`);
-  }
-  // eslint-disable-next-line no-useless-catch
+  console.log(`新しい本が追加されました: ${result.lastID}`);
 } catch (err) {
-  // 意図しないエラーが発生した場合に再送出する
-  throw err;
-} finally {
-  await closePromise(db);
+  if (err.code === "SQLITE_CONSTRAINT") {
+    console.error(`新しい本を追加する時のエラー: ${err.message}`);
+  } else {
+    // 想定しないエラーは再送出する
+    throw err;
+  }
 }
+try {
+  // 意図的にエラーを発生させるために、存在しないusersを参照する
+  const row = await getPromise(db, "SELECT * FROM users");
+  console.log(`取得した本: ${row.title}`);
+} catch (err) {
+  if (err.code === "SQLITE_ERROR") {
+    console.error(`本を取得する時のエラー: ${err.message}`);
+  } else {
+    // 想定しないエラーは再送出する
+    throw err;
+  }
+}
+
+await closePromise(db);
