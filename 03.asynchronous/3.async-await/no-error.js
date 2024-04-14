9a3e94affa9b22a8ee0d05074542dfce8871272c
hiromisugie <lozz.gggg@gmail.com>
2024-04-14 15:41:39 +0900

    エラーハンドリングしないのでtry...catch構文を削除

--- 03.asynchronous/3.async-await/no-error.js
+++ 03.asynchronous/3.async-await/no-error.js
@@ -3,22 +3,18 @@ import { runPromise, getPromise, closePromise } from "../db-operations.js";
 
 const db = new sqlite3.Database(":memory:");
 
-try {
-  await runPromise(
-    db,
-    "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-  );
-  const result = await runPromise(
-    db,
-    "INSERT INTO books(title) VALUES(?)",
-    "独習JavaScript",
-  );
-  console.log(`新しい本が追加されました: ${result.lastID}`);
+await runPromise(
+  db,
+  "CREATE TABLE books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+);
+const result = await runPromise(
+  db,
+  "INSERT INTO books(title) VALUES(?)",
+  "独習JavaScript",
+);
+console.log(`新しい本が追加されました: ${result.lastID}`);
 
-  const row = await getPromise(db, "SELECT * FROM books");
-  if (row) {
-    console.log(`取得した本: ${row.title}`);
-  }
-} finally {
-  await closePromise(db);
-}
+const row = await getPromise(db, "SELECT * FROM books");
+console.log(`取得した本: ${row.title}`);
+
+await closePromise(db);
