97f0e48d3886ed5f827cad6828bb6b65fab6545d
hiromisugie <lozz.gggg@gmail.com>
2024-05-21 05:04:22 +0900

    tryとfinallyを削除

--- 04.class/src/command-manager.js
+++ 04.class/src/command-manager.js
@@ -9,35 +9,32 @@ export class CommandManager {
   }
 
   async handleCommands() {
-    try {
-      if (argv.length === ARG_LENGTH_WITHOUT_COMMAND) {
-        console.log("Enter memo (Ctrl+d to exit):");
-        let content = "";
-        process.stdin.on("data", (data) => {
-          content += data;
-        });
-        await new Promise((resolve) => process.stdin.on("end", resolve));
-        await this.memoManager.addMemo(content.trim());
-      } else if (argv.length === ARG_LENGTH_WITH_COMMAND) {
-        const command = argv[2];
-        switch (command) {
-          case "-l":
-            await this.memoManager.listMemos();
-            break;
-          case "-r":
-            await this.memoManager.readMemo();
-            break;
-          case "-d":
-            await this.memoManager.deleteMemo();
-            break;
-          default:
-            console.error("Execute with l, r, or d option.");
-        }
-      } else {
-        console.error("Only one option can be specified.");
+    if (argv.length === ARG_LENGTH_WITHOUT_COMMAND) {
+      console.log("Enter memo (Ctrl+d to exit):");
+      let content = "";
+      process.stdin.on("data", (data) => {
+        content += data;
+      });
+      await new Promise((resolve) => process.stdin.on("end", resolve));
+      await this.memoManager.addMemo(content.trim());
+    } else if (argv.length === ARG_LENGTH_WITH_COMMAND) {
+      const command = argv[2];
+      switch (command) {
+        case "-l":
+          await this.memoManager.listMemos();
+          break;
+        case "-r":
+          await this.memoManager.readMemo();
+          break;
+        case "-d":
+          await this.memoManager.deleteMemo();
+          break;
+        default:
+          console.error("Execute with l, r, or d option.");
       }
-    } finally {
-      await this.memoManager.databaseManager.close();
+    } else {
+      console.error("Only one option can be specified.");
     }
+    await this.memoManager.databaseManager.close();
   }
 }
