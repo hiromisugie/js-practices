856f246e3b982f265620f706a6940182bd8212c3
hiromisugie <lozz.gggg@gmail.com>
2024-05-25 04:27:51 +0900

    rows.length === 0の処理を早期リターンに修正

--- 04.class/src/memo-app.js
+++ 04.class/src/memo-app.js
@@ -32,12 +32,12 @@ export class MemoApp {
       const rows = await this.fetchMemos();
       if (rows.length === 0) {
         console.log("No memo has been registered yet.");
-      } else {
-        rows.forEach((row) => {
-          const firstLine = this.getFirstLine(row.memo);
-          console.log(`${row.memo.substring(0, firstLine)}`);
-        });
+        return;
       }
+      rows.forEach((row) => {
+        const firstLine = this.getFirstLine(row.memo);
+        console.log(`${row.memo.substring(0, firstLine)}`);
+      });
     } catch (err) {
       console.error(`Error when displaying memo list: ${err.message}`);
     }
@@ -48,29 +48,29 @@ export class MemoApp {
       const rows = await this.fetchMemos();
       if (rows.length === 0) {
         console.log("No memo has been registered yet.");
-      } else {
-        const choices = rows.map((row) => {
-          const firstLine = this.getFirstLine(row.memo);
-          return {
-            name: `${row.memo.substring(0, firstLine)}`,
-            value: row.id,
-          };
-        });
-        const prompt = new Select({
-          name: "memo",
-          message: "Choose a memo you want to see:",
-          choices: choices,
-          result(names) {
-            return this.map(names);
-          },
-        });
-
-        const db = await this.dbPromise;
-        const selectedId = await prompt.run();
-        const sql = "SELECT memo FROM memos WHERE id = ?";
-        const memo = await db.get(sql, [parseInt(Object.values(selectedId))]);
-        console.log(memo ? memo.memo : "Memo not found.");
+        return;
       }
+      const choices = rows.map((row) => {
+        const firstLine = this.getFirstLine(row.memo);
+        return {
+          name: `${row.memo.substring(0, firstLine)}`,
+          value: row.id,
+        };
+      });
+      const prompt = new Select({
+        name: "memo",
+        message: "Choose a memo you want to see:",
+        choices: choices,
+        result(names) {
+          return this.map(names);
+        },
+      });
+
+      const db = await this.dbPromise;
+      const selectedId = await prompt.run();
+      const sql = "SELECT memo FROM memos WHERE id = ?";
+      const memo = await db.get(sql, [parseInt(Object.values(selectedId))]);
+      console.log(memo ? memo.memo : "Memo not found.");
     } catch (err) {
       console.error(`Error when displaying memo details: ${err.message}`);
     }
@@ -81,29 +81,29 @@ export class MemoApp {
       const rows = await this.fetchMemos();
       if (rows.length === 0) {
         console.log("No memo has been registered yet.");
-      } else {
-        const choices = rows.map((row) => {
-          const firstLine = this.getFirstLine(row.memo);
-          return {
-            name: `${row.memo.substring(0, firstLine)}`,
-            value: row.id,
-          };
-        });
-        const prompt = new Select({
-          name: "memo",
-          message: "Choose a memo you want to delete:",
-          choices: choices,
-          result(names) {
-            return this.map(names);
-          },
-        });
-
-        const db = await this.dbPromise;
-        const selectedId = await prompt.run();
-        const sql = "DELETE FROM memos WHERE id = ?";
-        await db.run(sql, [parseInt(Object.values(selectedId))]);
-        console.log("Deleted memo.");
+        return;
       }
+      const choices = rows.map((row) => {
+        const firstLine = this.getFirstLine(row.memo);
+        return {
+          name: `${row.memo.substring(0, firstLine)}`,
+          value: row.id,
+        };
+      });
+      const prompt = new Select({
+        name: "memo",
+        message: "Choose a memo you want to delete:",
+        choices: choices,
+        result(names) {
+          return this.map(names);
+        },
+      });
+
+      const db = await this.dbPromise;
+      const selectedId = await prompt.run();
+      const sql = "DELETE FROM memos WHERE id = ?";
+      await db.run(sql, [parseInt(Object.values(selectedId))]);
+      console.log("Deleted memo.");
     } catch (err) {
       console.error(`Error when deleting a memo: ${err.message}`);
     }
